300e057890455bda1eff698d131fc6bde6322553
Marcus <genestonmarcus@gmail.com>
2026-01-17 03:03:06 +0800

    refactor(all): start full project rebuild from scratch

---
+++ src/main/java/org/example/cafe828rebuild/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.cafe828rebuild;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
