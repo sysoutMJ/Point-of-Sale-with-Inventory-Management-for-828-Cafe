4f6e35fdbcf3802d2a70fc31c48bf6fc61947157
Marcus <genestonmarcus@gmail.com>
2026-01-18 03:32:25 +0800

    chore: add sidebar directory in src/main/java/org/example/cafe828rebuild

---
+++ src/main/java/org/example/cafe828rebuild/sidebar/SideBarController.java
@@ -0,0 +1,11 @@
+package org.example.cafe828rebuild.sidebar;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+public class SideBarController {
+
+    @FXML private Pane sideBarPane;
+
+
+}
