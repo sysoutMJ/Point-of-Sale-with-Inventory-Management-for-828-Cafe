c0db4219441cf806fa4dea26fa819682ddb3356f
Marcus <genestonmarcus@gmail.com>
2026-01-18 06:56:08 +0800

    feat(sidebar): integrate sidebar and add hide method
    - Added sidebar.fxml to main-application.fxml via <fx:include>
    - Declared container nodes in MainController for sidebar
    - Added hideSideBarVBoxContainer() method to hide the sidebar dynamically
    - Fixed alignment of the sidebar VBox container
    - Enables sidebar to be shown/hidden without layout issues

--- src/main/java/org/example/cafe828rebuild/MainController.java
+++ src/main/java/org/example/cafe828rebuild/MainController.java
@@ -1,14 +1,25 @@
 package org.example.cafe828rebuild;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private VBox sideBarVBoxContainer;
+
+    @FXML
+    private StackPane mainStackPaneContainer;
+
+    void hideSideBarVBoxContainer(){
+        sideBarVBoxContainer.setVisible(false);
+        sideBarVBoxContainer.setManaged(false);
     }
+
+    public void initialize(){
+        System.out.println("MainController initialized!");
+        hideSideBarVBoxContainer();
+    }
+
 }
